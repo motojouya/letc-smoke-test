b0ca13053e6cf8babc6969f558b506762bf0cc51
motojouya <motojouya@gmail.com>
2018-06-15 20:31:10 +0900

    add config template

--- app/config.js
+++ app/config.js
@@ -1,2 +1,24 @@
+/*
+ * {
+ *   dbDriver: 'required letc-pg etc',
+ *   static: 'option string directory',
+ *   topLevelCatch: 'option function',
+ *   logger: 'option object',
+ *   logFormat: 'option string',
+ *   requestParser: 'option array[object]',
+ *   session: { option
+ *     sessionSecret: 'required',
+ *     ..parameterForSession,
+ *   },
+ *   auth: {
+ *     check : 'function',
+ *     router: 'if authorized path, here',
+ *   },
+ * }
+ */
+
+
 module.export = {
+
+
 };
